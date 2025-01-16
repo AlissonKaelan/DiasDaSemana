cd650aa99af37b4427a434de2b762aff089d0756
AlissonKaelan <alissonkaelanc@gmail.com>
2025-01-15 21:41:51 -0300

    Update Project

---
+++ DiasDaSemana/src/DiaDaSemana/DiaDaSemanaCondicoes.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package DiaDaSemana;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alisson Kaelan
+ */
+public class DiaDaSemanaCondicoes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+      
+        System.out.println("Qaul o dia na semana em Numero?");
+        int dia = s.nextInt();
+        //switch so serve para numeros Inteiros
+        switch (dia) {
+            case 1:
+                System.out.println("Segunda-feira");
+                break;
+            case 2:
+                System.out.println("Terça-feira");
+                break;
+            case 3:
+                System.out.println("Quarta-feira");
+                break;
+            case 4:
+                System.out.println("Quinta-feira");
+                break;
+            case 5:
+                System.out.println("Sexta-feira");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("Dia inválido");
+        }
+    }
+
+        
+        
+    }
+    
+
